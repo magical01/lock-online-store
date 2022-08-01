f700b6097d752d7fadd95bff457f48d8c4f23283
magical01 <denis.verzakov.015@gmail.com>
2022-08-01 17:38:22 +0500

    feat: add product section

---
+++ src/js/components/popular-slider.js
@@ -0,0 +1,13 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+
+const popularSlider = new Swiper('.popular__slider', {
+  loop: true,
+  slidesPerView: '4',
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.popular-btn-next',
+    prevEl: '.popular-btn-prev'
+  }
+});
