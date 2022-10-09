d496749bad7b1cf5c1f1b35e7bf0ff2a7d4f33b9
magical01 <denis.verzakov.015@gmail.com>
2022-10-09 19:51:37 +0500

    feat: finish adaptive for index html

--- src/js/components/popular-slider.js
+++ src/js/components/popular-slider.js
@@ -11,4 +11,36 @@ const popularSlider = new Swiper('.popular__slider', {
     prevEl: '.popular-btn-prev'
   },
   speed: 500,
+  breakpoints: {
+    // when window width is >= 320px
+    1024: {
+      slidesPerView: '4',
+      spaceBetween: 30
+    },
+    695: {
+      slidesPerView: '3',
+      spaceBetween: 25
+    },
+    476: {
+      slidesPerView: '2',
+      spaceBetween: 25
+    },
+    320: {
+      slidesPerView: '1',
+    },
+
+    200: {
+      slidesPerView: '1',
+    },
+    // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30
+    // },
+    // // when window width is >= 640px
+    // 640: {
+    //   slidesPerView: 4,
+    //   spaceBetween: 40
+    // }
+  }
 });
